fc9b9f329a9ab0d0d7fb3e57876dfa32c69c1ed3
ryu-man <rioman19@gmail.com>
2021-01-26 10:00:39 +0100

    Rename variable Code clean up

--- src/bubble_chart.svelte
+++ src/bubble_chart.svelte
@@ -1,31 +1,25 @@
 <script lang="ts">
-  import { onMount, setContext, tick } from 'svelte'
-  import {
-    extent,
-    group,
-    scaleLinear,
-    scaleTime,
-    scaleOrdinal,
-    schemeCategory10
-  } from 'd3'
+  import { setContext } from 'svelte'
+  import { extent, scaleLinear } from 'd3'
   import { XAxis, YAxis, Grid } from './components'
-  import { css } from './utils'
   import type { ScaleLinear } from 'd3'
   import Chartist from './chartist.svelte'
   import { key } from './context.svelte'
   import { writable } from 'svelte/store'
   import Circle from './components/circle.svelte'
+  import AnimatedCircle from './components/animated_circle.svelte'
 
   export let width: number
   export let height: number
   export let margin
-  export let data: any[] = []
   export let groupBy: (d: any) => any
   export let curve
   export let colorRange: readonly string[]
   export let style = {}
-  export let zDomain: any[]
+  export let data: any[] = []
+
   export let zAccessor: (d: any) => any = () => ''
+  export let zDomain: any[]
   export let zRange: [number, number]
 
   let zScale: ScaleLinear<number, number> = scaleLinear()
@@ -34,13 +28,12 @@
     zScale
   })
   setContext(key, context)
-  
-  zDomain = zDomain || extent(data, zAccessor)
-  zRange = zRange || [4, innerHeight * 0.1]
-  zScale.domain(zDomain).range(zRange)
-  
-  function init(node: HTMLElement, data: Array<any>) {
 
+  zDomain = zDomain || extent(data, zAccessor)
+  zRange = zRange || [2, innerHeight * 0.1]
+  zScale.domain(zDomain).range(zRange)
+
+  function init(node: HTMLElement, data: Array<any>) {
     return {
       update(data: Array<any>) {
         zDomain = extent(data, zAccessor)
@@ -61,35 +54,31 @@
   {groupBy}
   {colorRange}
   {style}
-  let:samples
-  let:colorScale>
-
-  <slot name="content" slot="content" test={true}>
-    <XAxis class="x" position="bottom" />
-    <YAxis
-      class="y"
-      format="~s"
-      position="right" />
+  let:entries
+  let:colorScale
+>
+  <slot name="content" slot="content">
+    <XAxis position="bottom" />
+    <YAxis format="~s" position="right" />
     <Grid />
     <!-- {#if keys.length > 1}
           <Legend right="1vw" top="1vw" />
         {/if} -->
   </slot>
-  
+
   <g>
-    {#each samples as entry}
+    {#each entries as entry}
       {#each entry[1] as item}
         <slot>
-          <Circle
+          <AnimatedCircle
             {item}
             r={zScale(zAccessor(item))}
-            fill={colorScale(entry[0])} />
+            fill={colorScale(entry[0])}
+          />
         </slot>
       {/each}
     {/each}
   </g>
-
-  
 </Chartist>
 
 <style>
