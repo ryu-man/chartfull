5507c82a936da41de226d2f34166e44d7e1ce0b9
ryu-man <rioman19@gmail.com>
2021-03-02 10:57:44 +0100

    refactor: Add slot & code clean up

--- src/components/rect.svelte
+++ src/components/rect.svelte
@@ -1,50 +1,32 @@
 <script>
-  import { cubicIn } from 'svelte/easing'
-  import { fade } from 'svelte/transition'
-  import { tweened } from 'svelte/motion'
-  import { css } from '../utils'
-
-  import { easeBackIn } from 'd3'
   import { graficoContext } from '../context.svelte'
 
-  /* type EasingFunction = (t: number) => number
-  interface TransitionParams {
-    delay?: number
-    duration?: number
-    easing?: EasingFunction
-  }
-  interface TransitionConfig {
-    delay?: number
-    duration?: number
-    easing?: EasingFunction
-    css?: (t: number, u: number) => string
-    tick?: (t: number, u: number) => void
-  } */
+  const { xScale, yScale } = graficoContext()
 
-  /* export let width: number
-  export let height: number
-  export let x: number
-  export let y: number */
-  const { xScale, yScale, innerHeight } = graficoContext()
+  export let x1
+  export let x2
+  export let y1
+  export let y2
 
-  export let x
-  export let width
-  export let y
-  export let height
-  export let fill = 'black'
-  export let style = {}
-
-  // console.log(xScale(x1))
-  // console.log(x1, x2, y1, y2)
-
-  /* $: x = $xScale(x1)
-  $: y = $yScale(y1)
-  $: width = Math.abs($xScale(x2) - $xScale(x1))
-  $: height = Math.abs($yScale(y2) - $yScale(y1)) */
-  // $:console.log(x,y,width,height)
+  $: _x1 = $xScale(x1)
+  $: _x2 = $xScale(x2)
+  $: _y1 = $yScale(y1)
+  $: _y2 = $yScale(y2)
 </script>
 
-<rect use:css={style} {x} {y} {width} {height} {fill} />
+<slot
+  x={Math.min(_x1, _x2)}
+  y={Math.min(_y1, _y2)}
+  width={Math.abs(_x2 - _x1)}
+  height={Math.abs(_y2 - _y1)}
+>
+  <rect
+    x={Math.min(_x1, _x2)}
+    y={Math.min(_y1, _y2)}
+    width={Math.abs(_x2 - _x1)}
+    height={Math.abs(_y2 - _y1)}
+  />
+</slot>
 
 <style>
   rect {
