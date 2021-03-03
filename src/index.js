e16927587af11972f5cab62f357af4fb50d19e61
ryu-man <rioman19@gmail.com>
2021-03-03 22:29:24 +0100

    refactor: Use named export instead of export *

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-export * from './components'
+export {
+  Area,
+  Circle,
+  Grid,
+  GridLine,
+  Legend,
+  Line,
+  Pie,
+  Rect,
+  Scales,
+  Slice,
+  XAxis,
+  XTick,
+  YAxis,
+  YTick
+} from './components'
 export { default as LineChart } from './line_chart.svelte'
 export { default as PieChart } from './pie_chart.svelte'
 export { default as BarChart } from './bar_chart.svelte'
@@ -7,4 +22,3 @@ export { default as AreaChart } from './area_chart.svelte'
 export { default as Histogram } from './histogram.svelte'
 export { default as Heatmap } from './heatmap.svelte'
 export { default as BubbleChart } from './bubble_chart.svelte'
-
