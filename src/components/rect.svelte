3e597dc7e0611cc982d1d4b78c7dea41be99f75a
ryu-man <rioman19@gmail.com>
2021-03-08 18:13:05 +0100

    refactor: Set better default values fo x2 & y2

--- src/components/rect.svelte
+++ src/components/rect.svelte
@@ -1,30 +1,33 @@
 <script>
   import { graficoContext } from '../context.svelte'
-
+  import { min } from 'd3-array'
   const { xScale, yScale } = graficoContext()
 
   export let x1
   export let y1
-  export let x2 = 0
-  export let y2 = 0
+  export let x2 = $xScale.bandwidth
+    ? x1
+    : min($xScale.domain())
+  export let y2 = $yScale.bandwidth
+    ? y1
+    : min($yScale.domain())
 
   $: _x1 = $xScale(x1) || 0
   $: _y1 = $yScale(y1) || 0
   $: _x2 = $xScale(x2) || 0
   $: _y2 = $yScale(y2) || 0
-
 </script>
 
 <slot
   x={Math.min(_x1, _x2)}
   y={Math.min(_y1, _y2)}
-  width={$xScale?.bandwidth?.() ?? Math.abs(_x2 - _x1)}
+  width={Math.abs(_x2 - _x1)}
   height={Math.abs(_y2 - _y1)}
 >
   <rect
     x={Math.min(_x1, _x2)}
     y={Math.min(_y1, _y2)}
-    width={$xScale?.bandwidth?.() ?? Math.abs(_x2 - _x1)}
+    width={Math.abs(_x2 - _x1)}
     height={Math.abs(_y2 - _y1)}
   />
 </slot>
