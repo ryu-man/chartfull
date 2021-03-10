aa018fa01a41a5d145bb5b15f1cd860be36bc062
ryu-man <rioman19@gmail.com>
2021-03-11 00:10:19 +0100

    refactor: Compatibility Improvements

--- src/heatmap.svelte
+++ src/heatmap.svelte
@@ -1,99 +1,73 @@
 <script>
-  import { setContext, tick } from 'svelte'
-  import { css } from './utils'
-  import { scaleOrdinal } from 'd3-scale'
-  import { schemeAccent } from 'd3-scale-chromatic'
-  import { bin, extent, max, group } from 'd3-array'
-  import { XAxis, YAxis} from './components'
-  import { fade, fly } from 'svelte/transition'
+  import { XAxis, YAxis } from './components'
   import Grafico from './grafico.svelte'
-  import Grid from './components/grid/grid.svelte'
-  import { key } from './context.svelte'
-  import Bin from './components/bin.svelte'
+  import Context, { graficoContext } from './context.svelte'
   import { writable } from 'svelte/store'
+  import { scaleBand, scaleLinear } from 'd3-scale'
 
   export let width = 600
   export let height = 400
   export let padding = {}
   export let data = []
-
   export let colorAccessor = () => ''
-
   export let colorDomain = [1, 100]
   export let colorRange = ['white', '#69b3a2']
   export let style = {}
 
-  let colorScale
+  const {
+    xAccessor = writable((d) => d.x),
+    yAccessor = writable((d) => d.y),
+    xScale = writable(scaleBand()),
+    yScale = writable(scaleBand()),
+    colorScale = writable(scaleLinear())
+  } = graficoContext()
+
+  $: $colorScale.domain(colorDomain).range(colorRange),
+    ($colorScale = $colorScale)
 </script>
 
-<Grafico
-  class="heat"
-  {width}
-  {height}
-  {padding}
-  {data}
-  {colorRange}
-  {style}
-  let:entries
-  let:colorScale
->
-  <slot name="xaxis" slot="xaxis">
-    <XAxis position="bottom" />
-  </slot>
-  <slot name="yaxis" slot="yaxis">
-    <YAxis position="right" />
-  </slot>
-  <slot name="grid" slot="grid">
-    <Grid />
-  </slot>
-  <slot name="legend" slot="legend" />
+<Context value={{ xAccessor, yAccessor, xScale, yScale }}>
+  <Grafico
+    class="heat"
+    {width}
+    {height}
+    {padding}
+    {data}
+    {colorRange}
+    {style}
+  >
+    <slot name="xaxis" slot="xaxis">
+      <XAxis scale={$xScale} />
+    </slot>
+    <slot name="yaxis" slot="yaxis">
+      <YAxis scale={$yScale} />
+    </slot>
+    <slot name="grid" slot="grid">
+      <!-- <Grid /> -->
+    </slot>
+    <slot name="legend" slot="legend" />
 
-  <g>
-    {#each data as item, i}
-      <slot>
-        <Bin
-          {item}
-          style={{
-            fill: colorScale(colorAccessor(item)),
-            stroke: '#fff',
-            strokeWidth: '2px'
-          }}
-        />
-      </slot>
-    {/each}
-  </g>
-</Grafico>
+    <g>
+      {#each data as item, i}
+        <slot
+          x={$xScale($xAccessor(item))}
+          y={$yScale($yAccessor(item))}
+          xBandwidth={$xScale.bandwidth()}
+          yBandwidth={$yScale.bandwidth()}
+          color={$colorScale(colorAccessor(item))}
+        >
+          <rect
+            x={$xScale($xAccessor(item))}
+            y={$yScale($yAccessor(item))}
+            width={$xScale.bandwidth()}
+            height={$yScale.bandwidth()}
+            fill={$colorScale(colorAccessor(item))}
+          />
+        </slot>
+      {/each}
+    </g>
+  </Grafico>
+</Context>
 
-<!-- <div class="heatmap charter" use:init="{data}">
-  {#await tick() then value}
-    <Chartist>
-      {#await tick() then value}
-        
-      {/await}
-      <div slot="content">
-        <Grid />
-        <XAxis
-          class="x"
-          style="{{ width: '100%', top: '100%', padding: '1% 0 0 0',transform: `translateX(${xScale.bandwidth() / 2}px)` }}"
-        />
-        <YAxis
-          class="y"
-          format="~s"
-          style="{{ height: '100%', right: '100%', padding: '0 1% 0 0', transform: `translateY(${yScale.bandwidth() / 2}px)` }}"
-        />
-      </div>
-    </Chartist>
-  {/await}
-</div> -->
 <style>
-  .heatmap {
-    overflow: hidden;
-  }
-  .charter {
-    width: auto;
-    min-width: 300px;
-    height: auto;
-    min-height: 300px;
-    position: relative;
-  }
 </style>
