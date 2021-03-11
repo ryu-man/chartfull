0a81349ade71538f96e277682aebc0a978e9f8dd
ryu-man <rioman19@gmail.com>
2021-03-11 10:55:16 +0100

    refactor: Add scatter plot context

--- src/scatter_plot.svelte
+++ src/scatter_plot.svelte
@@ -1,5 +1,6 @@
 <script>
   import { XAxis, YAxis, Grid, Line, Circle } from './components'
+  import Context, { graficoContext } from './context.svelte'
   import Grafico from './grafico.svelte'
 
   export let width = 600
@@ -9,54 +10,62 @@
   export let groupBy = () => ''
   export let radius = 8
   export let connected = false
-
   export let style = {}
+
+  const {
+    xAccessor = writable((d) => d.x),
+    yAccessor = writable((d) => d.y),
+    xScale = writable(scaleLinear()),
+    yScale = writable(scaleLinear())
+  } = graficoContext()
 </script>
 
-<Grafico
-  class="scatter"
-  {width}
-  {height}
-  {padding}
-  {data}
-  {groupBy}
-  {style}
-  let:entries
-  let:colorScale
->
-  <slot name="xaxis" slot="xaxis">
-    <XAxis position="bottom" />
-  </slot>
-  
-  <slot name="yaxis" slot="yaxis">
-    <YAxis />
-  </slot>
+<Context value={{ xAccessor, yAccessor, xScale, yScale }}>
+  <Grafico
+    class="scatter"
+    {width}
+    {height}
+    {padding}
+    {data}
+    {groupBy}
+    {style}
+    let:entries
+    let:colorScale
+  >
+    <slot name="xaxis" slot="xaxis">
+      <XAxis scale={$xScale} />
+    </slot>
 
-  <slot name="grid" slot="grid">
-    <Grid />
-  </slot>
+    <slot name="yaxis" slot="yaxis">
+      <YAxis scale={$yScale} />
+    </slot>
 
-  <slot name="legend" slot="legend" />
+    <slot name="grid" slot="grid">
+      <Grid />
+    </slot>
 
-  <slot name="title" slot="title" />
+    <slot name="legend" slot="legend" />
 
-  <g>
-    {#each entries as entry}
-      <g>
-        {#if connected}
-          <slot name="line" lineData={entry[1]}>
-            <Line data={entry[1]} style={{ stroke: colorScale(entry[0]) }} />
-          </slot>
-        {/if}
-        {#each entry[1] as item, i}
-          <slot index={i} color={colorScale(groupBy(entry[0]))}>
-            <Circle index={i} duration={i * 96} r={radius} />
-          </slot>
-        {/each}
-      </g>
-    {/each}
-  </g>
-</Grafico>
+    <slot name="title" slot="title" />
+
+    <g>
+      {#each entries as entry}
+        <g>
+          {#if connected}
+            <slot name="line" lineData={entry[1]}>
+              <Line data={entry[1]} style={{ stroke: colorScale(entry[0]) }} />
+            </slot>
+          {/if}
+          {#each entry[1] as item, i}
+            <slot index={i} color={colorScale(groupBy(entry[0]))}>
+              <Circle index={i} duration={i * 96} r={radius} />
+            </slot>
+          {/each}
+        </g>
+      {/each}
+    </g>
+  </Grafico>
+</Context>
 
 <style>
 </style>
