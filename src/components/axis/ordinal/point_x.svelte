dccadc942f6d0b2d4cce7c2ea203f897c357a433
ryu-man <rioman19@gmail.com>
2021-03-11 08:51:07 +0100

    feat: Add continous point scale axises

---
+++ src/components/axis/ordinal/point_x.svelte
@@ -0,0 +1,74 @@
+<script>
+  import XAxis from '../x_axis.svelte'
+  import { scalePoint } from 'd3-scale'
+  import { graficoContext } from '../../../context.svelte'
+
+  const {
+    innerWidth,
+    innerHeight,
+    xScale,
+    xAccessor,
+    xTicks,
+    data,
+    bins,
+    defaultXDomain = (data, accessor, bins) => data.map(accessor),
+    defaultRange = (innerWidth, innerHeight) => [0, innerWidth]
+  } = graficoContext()
+
+  export let domain = defaultXDomain
+  export let range = defaultRange
+  export let round = false
+  export let padding
+  export let paddingInner
+  export let paddingOuter
+  export let align
+  export let step
+  export let bandwidth
+  export let tickValues
+
+  let _class
+  export { _class as class }
+  export let position
+  let scale = scalePoint()
+  export let accessor = $xAccessor
+  // $xAccessor = accessor
+
+  round && scale.round(round)
+  padding && scale.padding(padding)
+  paddingInner && scale.padding(paddingInner)
+  paddingOuter && scale.padding(paddingOuter)
+  align && scale.align(align)
+  step = scale.step()
+  bandwidth = scale.bandwidth()
+
+  let _tickValues =
+    typeof tickValues !== 'function'
+      ? (scale) => tickValues || scale.domain()
+      : tickValues
+  let formatter = (d) => d
+
+</script>
+
+<XAxis
+  class={_class}
+  {scale}
+  {accessor}
+  {domain}
+  {range}
+  {position}
+  tickValues={_tickValues}
+  let:index
+  let:tick
+  let:x
+  let:y
+  let:tickPosition
+>
+  <slot {tick} {index} {x} {y} {tickPosition}>
+    <span
+      use:tickPosition={{ x: (scale(tick) * 100) / $innerWidth, y: 0 }}
+      class="tick">{tick}</span
+    >
+  </slot>
+
+  <slot name="label" slot="label" />
+</XAxis>
---
+++ src/components/axis/ordinal/point_y.svelte
@@ -0,0 +1,73 @@
+<script>
+  import YAxis from '../y_axis.svelte'
+  import { scalePoint } from 'd3-scale'
+  import { graficoContext } from '../../../context.svelte'
+
+  const {
+    innerWidth,
+    innerHeight,
+    yAccessor,
+    yTicks,
+    data,
+    bins,
+    defaultYDomain = (data, accessor, bins) => data.map(accessor),
+    defaultRange = (innerWidth, innerHeight) => [0, innerHeight]
+  } = graficoContext()
+
+  export let domain = defaultYDomain
+  export let range = defaultRange
+  export let round = false
+  export let padding
+  export let paddingInner
+  export let paddingOuter
+  export let align
+  export let step
+  export let bandwidth
+  export let tickValues
+
+  let _class
+  export { _class as class }
+  export let position
+  let scale = scalePoint()
+  export let accessor = $yAccessor
+  // $yAccessor = accessor
+
+  round && scale.round(round)
+  padding && scale.padding(padding)
+  paddingInner && scale.padding(paddingInner)
+  paddingOuter && scale.padding(paddingOuter)
+  align && scale.align(align)
+  step = scale.step()
+  bandwidth = scale.bandwidth()
+
+  let _tickValues =
+    typeof tickValues !== 'function'
+      ? (scale) => tickValues || scale.domain()
+      : tickValues
+  let formatter = (d) => d
+
+</script>
+
+<YAxis
+  class={_class}
+  {scale}
+  {accessor}
+  {domain}
+  {range}
+  tickValues={_tickValues}
+  {position}
+  let:index
+  let:tick
+  let:x
+  let:y
+  let:tickPosition
+>
+<slot {tick} {index} {x} {y} {tickPosition}>
+  <span
+    use:tickPosition={{ y: (scale(tick) * 100) / $innerHeight, x: 0 }}
+    class="tick">{tick}</span
+  >
+</slot>
+
+  <slot name="label" slot="label" />
+</YAxis>
