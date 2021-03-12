6f58b49805e28297dc491a810143933a00059f2f
ryu-man <rioman19@gmail.com>
2021-03-12 23:03:59 +0100

    refactor: Better slots fallback

--- src/line_chart.svelte
+++ src/line_chart.svelte
@@ -23,28 +23,29 @@
   let:entries
   let:colorScale
 >
+  <slot name="xaxis" slot="xaxis">
+    <XAxis />
+  </slot>
 
-<slot name="xaxis" slot="xaxis">
-  <XAxis position="bottom" />
-</slot>
+  <slot name="yaxis" slot="yaxis">
+    <YAxis />
+  </slot>
 
-<slot name="yaxis" slot="yaxis">
-  <YAxis />
-</slot>
+  <slot name="grid" slot="grid">
+    <Grid />
+  </slot>
 
-<slot name="grid" slot="grid">
-  <Grid />
-</slot>
+  <slot name="legend" slot="legend" />
 
-<slot name="legend" slot="legend" />
+  <slot name="title" slot="title" />
 
-<slot name="title" slot="title" />
-
-<g>
-  {#each entries as [key, data]}
-    <slot {key} {data} color={colorScale(key)}>
-      <Line class={key} {data} {curve} style={{ stroke: colorScale(key) }} />
-    </slot>
-  {/each}
-</g>
+  <g>
+    {#each entries as [key, data]}
+      <slot {key} {data} color={colorScale(key)}>
+        <Line {data} {curve} let:d>
+          <path {d} fill="transparent" stroke={colorScale(key)} />
+        </Line>
+      </slot>
+    {/each}
+  </g>
 </Grafico>
