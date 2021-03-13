824e7036b4b0c25b4521800e3f4f7f07ea3a7663
ryu-man <rioman19@gmail.com>
2021-03-13 09:31:44 +0100

    refactor: Add line chart context

--- src/line_chart.svelte
+++ src/line_chart.svelte
@@ -1,5 +1,8 @@
 <script>
-  import { XAxis, YAxis, Legend, Grid, Line } from './components'
+  import { writable } from 'svelte/store'
+  import { scaleLinear } from 'd3-scale'
+  import { XAxis, YAxis, Grid, Line } from './components'
+  import Context, { graficoContext } from './context.svelte'
   import Grafico from './grafico.svelte'
 
   export let width
@@ -10,42 +13,59 @@
   export let curve
   export let colorRange
   export let style = {}
+
+  const {
+    xAccessor = writable((d) => d.x),
+    yAccessor = writable((d) => d.y),
+    xScale = writable(scaleLinear()),
+    yScale = writable(scaleLinear())
+  } = graficoContext()
 </script>
 
-<Grafico
-  {width}
-  {height}
-  {padding}
-  {data}
-  {groupBy}
-  {colorRange}
-  {style}
-  let:entries
-  let:colorScale
->
-  <slot name="xaxis" slot="xaxis">
-    <XAxis />
-  </slot>
+<Context value={{ xScale, yScale, xAccessor, yAccessor }}>
+  <Grafico
+    {width}
+    {height}
+    {padding}
+    {data}
+    {groupBy}
+    {colorRange}
+    {style}
+    let:entries
+    let:colorScale
+  >
+    <slot name="xaxis" slot="xaxis">
+      <XAxis scale={$xScale} accessor={$xAccessor} />
+    </slot>
 
-  <slot name="yaxis" slot="yaxis">
-    <YAxis />
-  </slot>
+    <slot name="yaxis" slot="yaxis">
+      <YAxis scale={$yScale} accessor={$xAccessor} />
+    </slot>
 
-  <slot name="grid" slot="grid">
-    <Grid />
-  </slot>
+    <slot name="grid" slot="grid">
+      <Grid />
+    </slot>
 
-  <slot name="legend" slot="legend" />
+    <slot name="legend" slot="legend" />
 
-  <slot name="title" slot="title" />
+    <slot name="title" slot="title" />
 
-  <g>
-    {#each entries as [key, data]}
-      <slot {key} {data} color={colorScale(key)}>
-        <Line {data} {curve} let:d>
-          <path {d} fill="transparent" stroke={colorScale(key)} />
-        </Line>
-      </slot>
-    {/each}
-  </g>
-</Grafico>
+    <g>
+      {#each entries as [key, data]}
+        <slot
+          {key}
+          {data}
+          color={colorScale(key)}
+          xScale={$xScale}
+          xAccessor={$xAccessor}
+          yScale={$yScale}
+          yAccessor={$yAccessor}
+        >
+          <Line {data} {curve} let:d>
+            <path {d} fill="transparent" stroke={colorScale(key)} />
+          </Line>
+        </slot>
+      {/each}
+    </g>
+  </Grafico>
+</Context>
