38352d5923f69c3be0111685f4a9df320ea108e2
ryu-man <rioman19@gmail.com>
2021-03-13 08:06:45 +0100

    refactor: Add area chart context

--- src/area_chart.svelte
+++ src/area_chart.svelte
@@ -1,5 +1,8 @@
 <script>
+  import { writable } from 'svelte/store'
+  import { scaleLinear } from 'd3-scale'
   import { Grid, Area, XAxis, YAxis } from './components'
+  import Context, { graficoContext } from './context.svelte'
   import Grafico from './grafico.svelte'
 
   export let width
@@ -10,47 +13,60 @@
   export let colorRange
 
   export let style = {}
+
+  const {
+    xAccessor = writable((d) => d.x),
+    yAccessor = writable((d) => d.y),
+    xScale = writable(scaleLinear()),
+    yScale = writable(scaleLinear())
+  } = graficoContext()
 </script>
 
-<Grafico
-  class="area"
-  {width}
-  {height}
-  {padding}
-  {data}
-  {groupBy}
-  {colorRange}
-  {style}
-  let:entries
-  let:colorScale
->
-  <slot name="xaxis" slot="xaxis">
-    <XAxis position="bottom" />
-  </slot>
+<Context value={{ xScale, xAccessor, yScale, yAccessor }}>
+  <Grafico
+    class="area"
+    {width}
+    {height}
+    {padding}
+    {data}
+    {groupBy}
+    {colorRange}
+    {style}
+    let:entries
+    let:colorScale
+  >
+    <slot name="xaxis" slot="xaxis">
+      <XAxis position="bottom" />
+    </slot>
 
-  <slot name="yaxis" slot="yaxis">
-    <YAxis format="~s" />
-  </slot>
+    <slot name="yaxis" slot="yaxis">
+      <YAxis format="~s" />
+    </slot>
 
-  <slot name="grid" slot="grid">
-    <Grid />
-  </slot>
+    <slot name="grid" slot="grid">
+      <Grid />
+    </slot>
 
-  <slot name="legend" slot="legend" />
+    <slot name="legend" slot="legend" />
 
-  <slot name="title" slot="title" />
+    <slot name="title" slot="title" />
 
-  <g>
-    {#each entries as entry}
-      <slot key={entry[0]} lineData={entry[1]} color={colorScale(entry[0])}>
-        <Area
-          data={entry[1]}
-          style={{ fill: colorScale(entry[0]), stroke: 'none' }}
-        />
-      </slot>
-    {/each}
-  </g>
-</Grafico>
-
-<style>
-</style>
+    <g>
+      {#each entries as [key, data]}
+        <slot
+          {key}
+          {data}
+          color={colorScale(key)}
+          xScale={$xScale}
+          xAccessor={$xAccessor}
+          yScale={$yScale}
+          yAccessor={$yAccessor}
+        >
+          <Area {data} let:d>
+            <path {d} fill={colorScale(key)} />
+          </Area>
+        </slot>
+      {/each}
+    </g>
+  </Grafico>
+</Context>
