6e979eafa31c0698c298f6b1588b1e72afb1134b
ryu-man <rioman19@gmail.com>
2021-03-20 11:02:40 +0100

    feat: Add Stack component

---
+++ src/components/stack.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { stack as Stack } from 'd3-shape'
+  export let data
+  export let keys = []
+  export let offset
+  export let order
+  export let value = []
+  const stack = Stack().keys(keys).offset(offset).order(order).value(value)
+
+  $: _ = stack(data)
+</script>
+
+<slot data={_} />
