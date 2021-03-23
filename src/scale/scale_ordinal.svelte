9f02d5f73f34dc9cf5cc44428284dd374b5609f9
ryu-man <rioman19@gmail.com>
2021-03-23 16:08:28 +0100

    feat: Initialize scale components

---
+++ src/scale/scale_ordinal.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { scaleOrdinal } from 'd3-scale'
+  import { storeScale } from './store_scale'
+
+  export let domain = []
+  export let range = [0, 1]
+  export let unknown
+
+  const scale = storeScale(scaleOrdinal())
+
+  $: scale.domain(domain)
+  $: scale.range(range)
+  $: unknown && scale.unknown(unknown)
+</script>
+
+<slot
+  scale={$scale}
+  {domain}
+  copy={scale.get.copy}
+  once={scale.get.once}
+  toString={scale.toString}
+/>
---
+++ src/scale/scale_pow.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { storeScale } from './store_scale'
+  import { scalePow } from 'd3-scale'
+
+  export let domain = []
+  export let range = [0, 1]
+  export let rangeRound
+  export let align
+  export let clamp = false
+  export let interpolate
+  export let nice
+  export let unknown
+  const scale = storeScale(scalePow())
+
+  $: scale.domain(domain)
+  $: scale.range(range)
+  $: clamp && scale.get.clamp(clamp)
+  $: interpolate && scale.get.interpolate(interpolate)
+  $: nice && scale.get.nice(nice)
+  $: unknown && scale.get.unknown(unknown)
+  $: rangeRound && scale.get.rangeRound(rangeRound)
+  $: align && scale.get.align(align)
+</script>
+
+<slot
+  scale={$scale}
+  {domain}
+  tickFormat={scale.get.tickFormat}
+  copy={scale.get.copy}
+  invert={scale.get.invert}
+  once={scale.get.once}
+  ticks={scale.get.ticks}
+  toString={scale.toString}
+/>
