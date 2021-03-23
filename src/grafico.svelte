7dc8b5a617b72260a9fd56c9e4a2cc6c0235328c
ryu-man <rioman19@gmail.com>
2021-03-23 10:29:02 +0100

    feat: Add support for multiple axises

--- src/grafico.svelte
+++ src/grafico.svelte
@@ -1,106 +1,88 @@
 <script>
   import { setContext, tick } from 'svelte'
   import { writable } from 'svelte/store'
-  import { scaleLinear, scaleOrdinal } from 'd3-scale'
-  import { schemeCategory10 } from 'd3-scale-chromatic'
-  import { group } from 'd3-array'
+  import { scaleLinear } from 'd3-scale'
   import { graficoContext, key } from './context.svelte'
-  import { css, getInnerSize } from './utils'
+  import { classNames, css } from './utils'
+  import { SizeStore } from './utils/size_store'
+  import { storeScale } from './scale/store_scale'
 
-  export let configure = basicConfiguration
+  export let init = basicConfiguration
   export let width = 300
   export let height = 300
   export let padding = {}
   export let data = []
-  export let groupBy = (d) => ''
-  export let colorRange = schemeCategory10
   export let style = {}
-  export let updateOnResize = false
+  export let onMount
+  export let onUpdate
+  export let onDestroy
   let _class = ''
   export { _class as class }
 
   padding = { top: 32, right: 32, bottom: 72, left: 72, ...padding }
-  let innerSize = getInnerSize({ width, height }, padding)
 
   const {
-    xAccessor = writable((d) => d.x),
-    yAccessor = writable((d) => d.y),
-    xScale = writable(scaleLinear()),
-    yScale = writable(scaleLinear()),
-    data: _data = writable(data),
-    entries = writable([]),
-    colorScale = writable(scaleOrdinal()),
-    width: _width = writable(width),
-    height: _height = writable(height),
-    innerWidth = writable(innerSize.innerWidth),
-    innerHeight = writable(innerSize.innerHeight),
-    zAccessor = writable((d) => d.z),
-    xTicks = writable(null),
-    yTicks = writable(null),
+    widthStore,
+    heightStore,
+    innerWidthStore,
+    innerHeightStore,
+    paddingStore
+  } = SizeStore(width, height, padding)
+
+  const {
+    xAccessors = {
+      default: (d) => d.x
+    },
+    yAccessors = {
+      default: (d) => d.y
+    },
+    xScales = {
+      default: storeScale(scaleLinear())
+    },
+    yScales = {
+      default: storeScale(scaleLinear())
+    },
+    dataStore = writable(data),
+    xTickValues = writable([]),
+    yTickValues = writable([]),
+    xAxisId = 'default',
+    yAxisId = 'default',
     ...rest
   } = graficoContext() ?? {}
 
   let context = {
-    colorScale,
-    width: _width,
-    height: _height,
-    innerWidth,
-    innerHeight,
-    margin: padding,
-    padding,
-    data: _data,
-    entries,
-    defaultXScale: scaleLinear,
-    defaultYScale: scaleLinear,
-    defaultZScale: scaleLinear,
-    xScale,
-    yScale,
-    xAccessor,
-    yAccessor,
-    zAccessor,
-    xTicks,
-    yTicks,
+    widthStore,
+    heightStore,
+    innerWidthStore,
+    innerHeightStore,
+    paddingStore,
+    dataStore,
+    xScales,
+    yScales,
+    xAccessors,
+    yAccessors,
+    xTickValues,
+    yTickValues,
+    xAxisId,
+    yAxisId,
     ...rest
   }
 
   setContext(key, context)
 
-  function init(node, data) {
-    return configure(node, data)
-  }
-
   function basicConfiguration(node, data = []) {
-    $_width = node.offsetWidth
-    $innerWidth = $_width - padding.left - padding.right
-    $innerHeight = $_height - padding.top - padding.bottom
-    if (typeof groupBy === typeof '') {
-      $entries = Array.from(group(data, (d) => d[groupBy]).entries())
-    } else {
-      $entries = Array.from(group(data, groupBy).entries())
-    }
+    $widthStore = node.offsetWidth
 
-    $colorScale.domain($entries.map((e) => e[0])).range(colorRange)
-
-    let resizeListener
-    if (updateOnResize) {
-      resizeListener = window.addEventListener('resize', (e) => {
-        $_width = node.offsetWidth
-        $innerWidth = $_width - padding.left - padding.right
-      })
-    }
+    onMount?.(node, data)
 
     return {
       update(data) {
-        $entries = Array.from(group(data, groupBy).entries())
+        $widthStore = node.offsetWidth
 
-        $_width = node.offsetWidth
-        $innerWidth = $_width - padding.left - padding.right
-        $innerHeight = $_height - padding.top - padding.bottom
-
-        $colorScale.domain($entries.map((e) => e[0]))
+        onUpdate?.(node, data)
       },
       destroy() {
-        resizeListener && window.removeEventListener(resizeListener)
+        onDestroy?.()
       }
     }
   }
@@ -109,39 +91,43 @@
 <figure
   use:css={style}
   use:init={data}
-  class="{_class && `${_class} `}grafico"
+  class={classNames(_class, 'grafico')}
   style={`--height:${height}px;`}
 >
+  <svg
+    height={$innerHeightStore}
+    viewBox="0 0 {$innerWidthStore} {$innerHeightStore}"
+    preserveAspectRatio="none"
+    style={`margin:${padding.top}px ${padding.right}px ${padding.bottom}px ${padding.left}px; width: calc(100% - ${padding.left}px - ${padding.right}px);`}
+  >
+    {#await tick() then value}
+      <slot name="grid" />
+      <slot
+        width={$widthStore}
+        height={$heightStore}
+        innerWidth={$innerWidthStore}
+        innerHeight={$innerHeightStore}
+        padding={$paddingStore}
+        {xScales}
+        {yScales}
+        {xAccessors}
+        {yAccessors}
+        {xAxisId}
+        {yAxisId}
+      />
+    {/await}
+  </svg>
   <div
     class="elements"
     style={`padding:${padding.top}px ${padding.right}px ${padding.bottom}px ${padding.left}px;`}
   >
     <div class="inner">
-      {#await tick() then value}
-        <slot name="xaxis" />
-        <slot name="yaxis" />
-        <slot name="legend" />
-      {/await}
+      <slot name="xaxis" />
+      <slot name="yaxis" />
+      <slot name="legend" />
     </div>
   </div>
-  <svg
-    {height}
-    viewBox="0 0 {$innerWidth} {$innerHeight}"
-    preserveAspectRatio="none"
-    style={`padding:${padding.top}px ${padding.right}px ${padding.bottom}px ${padding.left}px;`}
-  >
-    {#await tick() then value}
-      <slot name="grid" />
-      <slot
-        entries={$entries}
-        innerWidth={$innerWidth}
-        innerHeight={$innerHeight}
-        colorScale={$colorScale}
-        xScale={$xScale}
-        yScale={$yScale}
-      />
-    {/await}
-  </svg>
+
   <slot name="title" />
 </figure>
 
@@ -166,7 +152,7 @@
     left: 0;
     right: 0;
     bottom: 0;
-    z-index: -1;
+    z-index: 1;
   }
   .elements > .inner {
     position: relative;
