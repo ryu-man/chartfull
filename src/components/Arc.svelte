8ebd05287c21496346440d8079465520ffa22340
ryu-man <rioman19@gmail.com>
2021-04-01 11:48:35 +0100

    feat: Initialize Arc component

---
+++ src/components/Arc.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { arc as Arc } from 'd3-shape'
+
+  export let innerRadius = 0
+  export let outerRadius = 8
+  export let cornerRadius
+  export let context
+  export let endAngle
+  export let padAngle
+  export let padRadius
+  export let startAngle
+
+  let arc = Arc().innerRadius(innerRadius).outerRadius(outerRadius)
+
+  cornerRadius && arc.cornerRadius(cornerRadius)
+  context && arc.context(context)
+  endAngle && arc.endAngle(endAngle)
+  padAngle && arc.padAngle(padAngle)
+  padRadius && arc.padRadius(padRadius)
+  startAngle && arc.startAngle(startAngle)
+</script>
+
+<slot {arc} centroid={arc.centroid} toString={arc.toString} />
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export {
   Legend,
   Line,
   Pie,
+  Arc,
   Rect,
   Slice,
   Stack,
