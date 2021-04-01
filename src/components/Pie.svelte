4acb7493da33d3f3f752a23fc7739153e4eeaf45
ryu-man <rioman19@gmail.com>
2021-04-01 12:07:24 +0100

    refactor: Remove unused code & modify props

--- src/components/Pie.svelte
+++ src/components/Pie.svelte
@@ -1,72 +1,39 @@
 <script>
-  import { pie, arc as d3Arc } from 'd3-shape'
-  import { scaleOrdinal } from 'd3-scale'
-  import { schemeCategory10 } from 'd3-scale-chromatic'
-  import { writable } from 'svelte/store'
+  import { pie } from 'd3-shape'
   import Context, { graficoContext } from '../Context.svelte'
 
+  let context = {}
   const {
-    colorScale: _colorScale = writable(),
-    width,
-    height,
-    innerWidth,
-    innerHeight,
-    data,
-    // innerArc = writable(),
-    radius: _radius = writable(16),
-    innerRadius: _innerRadius = writable(0),
-    ...rest
-  } = graficoContext()
-  $_radius = Math.min($width, $height) / 2
+    widthStore,
+    heightStore,
+    innerWidthStore,
+    innerHeightStore
+  } = (context = graficoContext())
 
-  export let radius = $_radius
-  export let innerRadius = $_innerRadius
-  export let colorRange = schemeCategory10
-  export let key = (d) => d.key
+  export let data = []
   export let value = (d) => d.value
+  export let startAngle
+  export let endAngle
+  export let padAngle
+  export let sort
+  export let sortValues
 
-  radius && ($_radius = radius)
-  innerRadius && ($_innerRadius = innerRadius)
-  const context = {
-    // arc,
-    // innerArc,
-    radius: _radius,
-    innerRadius,
-    colorScale: _colorScale,
-    width,
-    height,
-    key,
-    value,
-    ...rest
-  }
+  context.value = value
 
-  const arc = d3Arc().innerRadius($_innerRadius).outerRadius($_radius)
-
-  // $innerArc = d3Arc().innerRadius($_radius).outerRadius($_radius)
-
-  $_colorScale = scaleOrdinal().domain($data.map(key)).range(colorRange)
-  const pieGen = pie().value(value)
+  const pies = pie().value(value)
+  startAngle && pies.startAngle(startAngle)
+  endAngle && pies.endAngle(endAngle)
+  padAngle && pies.padAngle(padAngle)
+  sort && pies.sort(sort)
+  sortValues && pies.sortValues(sortValues)
 </script>
 
 <Context value={context}>
-  <g transform={`translate(${$width / 2},${$height / 2})`}>
-    {#each pieGen($data) as arcData, i (key(arcData.data))}
-      <slot
-        data={arcData}
-        key={key(arcData.data)}
-        value={value(arcData.data)}
-        color={$_colorScale(key(arcData.data))}
-        index={i}
-      >
-        <path
-          class="slice"
-          d={arc(arcData)}
-          fill={$_colorScale(key(arcData.data))}
-        />
-      </slot>
-    {/each}
+  <g
+    transform={`translate(${($innerWidthStore || 0) / 2},${
+      ($innerHeightStore || 0) / 2
+    })`}
+  >
+    <slot data={pies(data)} {value} />
   </g>
 </Context>
-
-<style>
-</style>
