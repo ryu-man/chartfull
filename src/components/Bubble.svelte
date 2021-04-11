ac74785c63124fe17836904cf9071a5fd6b2fc3b
ryu-man <rioman19@gmail.com>
2021-04-11 23:33:33 +0100

    feat: Add z scale

--- src/components/Bubble.svelte
+++ src/components/Bubble.svelte
@@ -1,18 +1,25 @@
 <script>
   import { graficoContext } from '../Context.svelte'
-
+  import { scaleLinear } from 'd3-scale'
   const { xScales, yScales, xAccessors, yAccessors } = graficoContext()
 
   export let xAxisId = 'default'
   export let yAxisId = 'default'
   export let x = xAccessors[xAxisId]
   export let y = yAccessors[yAxisId]
+  export let domain = [0, 1]
+  export let range = [0, 1]
 
   xAccessors[xAxisId] = x
   yAccessors[yAxisId] = y
 
   const xScale = xScales[xAxisId]
   const yScale = yScales[yAxisId]
+  const zScale = scaleLinear(domain, range)
 </script>
 
-<slot xGet={(d) => $xScale(x(d))} yGet={(d) => $yScale(y(d))} />
+<slot
+  xGet={(d) => $xScale(x(d))}
+  yGet={(d) => $yScale(y(d))}
+  zGet={(d) => zScale(d)}
+/>
