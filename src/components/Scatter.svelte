9b3b99b9cfb24861f0853599b2018fafa4099f3e
ryu-man <rioman19@gmail.com>
2021-04-11 23:34:11 +0100

    refactor: Remove color scale

--- src/components/Scatter.svelte
+++ src/components/Scatter.svelte
@@ -1,15 +1,8 @@
 <script>
   import { graficoContext } from '../Context.svelte'
 
-  const {
-    xScales,
-    yScales,
-    xAccessors,
-    yAccessors,
-    colorScale
-  } = graficoContext()
+  const { xScales, yScales, xAccessors, yAccessors } = graficoContext()
 
-  export let id = 'default'
   export let xAxisId = 'default'
   export let yAxisId = 'default'
   export let x = xAccessors[xAxisId]
@@ -18,14 +11,8 @@
   xAccessors[xAxisId] = x
   yAccessors[yAxisId] = y
 
-  colorScale?.domain([...colorScale.domain(), id])
-
   const xScale = xScales[xAxisId]
   const yScale = yScales[yAxisId]
 </script>
 
-<slot
-  xGet={(d) => $xScale(x(d))}
-  yGet={(d) => $yScale(y(d))}
-  color={colorScale?.(id) ?? 'black'}
-/>
+<slot xGet={(d) => $xScale(x(d))} yGet={(d) => $yScale(y(d))} />
