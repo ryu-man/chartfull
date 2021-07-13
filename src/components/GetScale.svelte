c60b930d4164d2e851b6bfc9d6ce3d48354050f1
ryuman <rioman19@gmail.com>
2021-07-13 06:29:27 +0100

    Get scale by id from context

---
+++ src/components/GetScale.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { graficoContext } from '../Grafico.svelte'
+
+  const { scales } = graficoContext()
+
+  export let id
+
+  $: scale = Array.isArray(id) ? id.map((i) => $scales[i]) : $scales[id]
+</script>
+
+<slot {scale} />
