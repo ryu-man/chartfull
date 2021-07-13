f17a83ec2ca235e1d838f5d3d53d8bd0568371b2
ryuman <rioman19@gmail.com>
2021-07-13 18:42:39 +0100

    Refactor component

--- src/components/Bin.svelte
+++ src/components/Bin.svelte
@@ -1,26 +1,26 @@
 <script>
   import { bin as Bin } from 'd3-array'
-  import { graficoContext } from '../Context.svelte'
+  import { graficoContext } from '../Grafico.svelte'
 
-  const { xScales, yScales } = graficoContext()
+  const { scales } = graficoContext()
 
-  export let xAxisId = 'default'
-  export let yAxisId = 'default'
+  export let xId = 'x'
+  export let yId = 'y'
 
   // export let data = []
   export let domain = []
   export let value
   export let thresholds
 
-  const bin = Bin()
+  const root = Bin()
+  let bin = root
 
-  const xScale = xScales[xAxisId]
-  const yScale = yScales[yAxisId]
+  const xScale = $scales[xId]
+  const yScale = $scales[yId]
 
-  $: domain && bin.domain(domain)
-  $: value && bin.value(value)
-  $: thresholds && bin.thresholds(thresholds)
-
+  $: domain && (bin = root.domain(domain))
+  $: value && (bin = root.value(value))
+  $: thresholds && (bin = root.thresholds(thresholds))
 </script>
 
 <slot
