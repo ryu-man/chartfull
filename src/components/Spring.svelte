53a22491a6a808d8429339719ef1e8120014d403
ryuman <rioman19@gmail.com>
2021-07-13 06:30:02 +0100

    Use spring store as component

---
+++ src/components/Spring.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { spring } from 'svelte/motion'
+
+  export let from = 0
+  export let to = 0
+  export let stiffness = 0.15
+  export let damping = 0.8
+  export let precision = 0.001
+
+  const output = spring(from, {
+    stiffness,
+    precision,
+    damping
+  })
+
+  $: $output = to
+</script>
+
+<slot value={$output} />
