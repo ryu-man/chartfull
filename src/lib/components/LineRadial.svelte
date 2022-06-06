28f0fb203ab7380751ed3dbc8a35ee0ac2cad584
Abdelhalim Riache <rioman19@gmail.com>
2022-06-06 18:14:14 +0100

    d3 lineRadial component

---
+++ src/lib/components/LineRadial.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { lineRadial, curveLinearClosed } from 'd3';
+
+	export let radius;
+	export let angle;
+	export let data = [];
+
+	export let stroke = "currentColor"
+	export let fill = "none"
+
+	$: line = lineRadial().radius(radius).angle(angle).curve(curveLinearClosed);
+</script>
+
+<path d={line(data)} {fill} {stroke} />
