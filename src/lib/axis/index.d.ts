e047333044be80c5b9e3fbfb89cfd291eb97829f
Abdelhalim Riache <rioman19@gmail.com>
2022-06-06 17:43:44 +0100

    cleanup project

--- src/lib/axis/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as XAxis } from './XAxis.svelte'
-export { default as YAxis } from './YAxis.svelte'
-export { default as Tick } from './Tick.svelte'
--- src/lib/components/Call.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  export let func
-</script>
-
-<slot value={func($$restProps)} />
