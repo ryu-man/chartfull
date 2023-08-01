03b3e35971cb8c28db1abe04642e9ab7b9f3a278
abdelhalim <rioman19@gmail.com>
2023-08-01 12:09:19 +0100

    enhance code

--- src/lib/axis/RadialTick.svelte
+++ src/lib/axis/RadialTick.svelte
@@ -1,14 +1,9 @@
 <script>
 	import { format } from 'date-fns';
-	import { getContext } from 'svelte';
-	import { SCALES } from './RadialAxis.svelte';
-
-	console.log(getContext(SCALES));
-	const [currentScale$, previousScale$] = getContext(SCALES);
 
 	export let tick;
+	export let angle = 0;
 
-	$: angle = $currentScale$(tick);
 	$: sinSign = Math.sign(Math.sin(angle));
 </script>
 
@@ -17,11 +12,11 @@
 	text-anchor="middle"
 	transform="rotate({(angle * 180) / Math.PI}) translate({100}, 0)"
 >
-	<line x2={16} stroke="rgba(0 0 0 / .1)" />
+	<line x2={-8} stroke="rgba(0 0 0 / .1)" />
 	<slot>
 		<text
 			fill="rgba(0 0 0 / .3)"
-			transform="rotate({sinSign * -90}) translate(0,{sinSign * -16})"
+			transform="rotate({sinSign * -90}) translate(0,{sinSign * -24})"
 			dominant-baseline="middle"
 		>
 			{format(tick, 'MMM')}
