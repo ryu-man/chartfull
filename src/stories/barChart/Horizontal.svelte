b7c4cfda9c326d95a0e11932c5298f25561273e0
abdelhalim <rioman19@gmail.com>
2024-03-10 23:02:06 -0400

    enhance charts

--- src/stories/barChart/Horizontal.svelte
+++ src/stories/barChart/Horizontal.svelte
@@ -20,7 +20,7 @@
 
 	const [xAccessor, yAccessor] = [(d) => +d.Value, (d) => d.Country];
 
-	$: xScale = scaleLinear([0, max(data, xAccessor)], [196, innerWidth]);
+	$: xScale = scaleLinear([0, max(data, xAccessor)], [0, innerWidth]);
 	$: yScale = scaleBand(data.map(yAccessor), [innerHeight, 0]).padding(0.2);
 	$: colorScale = scaleOrdinal(data.map(yAccessor), schemeCategory10);
 
@@ -28,8 +28,8 @@
 	$: yGet = get(yScale, yAccessor);
 </script>
 
-<Chartfull padding={{ left: 0 }} bind:innerWidth bind:innerHeight {...args}>
-	<XAxis scale={xScale} y={innerHeight} orient="bottom" let:tick>
+<Chartfull padding={{ left: 0, top:8, bottom: 48 }} bind:innerWidth bind:innerHeight {...args}>
+	<XAxis scale={xScale} y={innerHeight} orient="bottom" tickArguments={[10, '.2f']} let:tick>
 		<Tick {tick} y2={-innerHeight} />
 
 		<text
@@ -37,39 +37,42 @@
 			x={innerWidth}
 			y={-innerHeight}
 			font-weight="700"
-			font-size="32pt"
+			font-size="24pt"
 			fill="black"
 			fill-opacity=".2"
-			letter-spacing="4pt"
 			text-anchor="end"
 			dominant-baseline="hanging">INCOME</text
 		>
 	</XAxis>
+
 	{#each data as item, i (i)}
 		{@const min_range = min(xScale.range())}
+		{@const max_range = max(xScale.range())}
+		{@const x = xGet(item)}
+		{@const width = x - min_range}
 
-		<Spring from={0} to={1} damping={0.5} stiffness={0.04} let:value={v}>
-			<g transform={`translate(${min_range},${yGet(item)})`} opacity={v}>
-				<text
-					dx={-16 * v}
-					dy={yScale.bandwidth() / 2}
-					text-anchor="end"
-					dominant-baseline="middle"
-					font-size="24"
-					font-weight="400"
-					fill={colorScale(yAccessor(item))}
-				>
-					{yAccessor(item)}
-				</text>
-				<Rect
-					width={(xGet(item) - min_range) * v}
-					height={yScale.bandwidth()}
-					fill={colorScale(yAccessor(item))}
-					fill-opacity=".3"
-					stroke={colorScale(yAccessor(item))}
-					r="0 2 2 0"
-				/>
-			</g>
-		</Spring>
+		<g transform={`translate(${min_range},${yGet(item)})`}>
+			<text
+				x={Math.min(width+16, max_range-164 )}
+				
+				dy={yScale.bandwidth() / 2}
+				text-anchor="start"
+				dominant-baseline="middle"
+				font-size="14pt"
+				font-weight="400"
+				fill={colorScale(yAccessor(item))}
+			>
+				{yAccessor(item)}
+			</text>
+
+			<Rect
+				width={width}
+				height={yScale.bandwidth()}
+				fill={colorScale(yAccessor(item))}
+				fill-opacity=".3"
+				stroke={colorScale(yAccessor(item))}
+				r="4"
+			/>
+		</g>
 	{/each}
 </Chartfull>
