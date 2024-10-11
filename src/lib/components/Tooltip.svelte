3f542b41dcfdb5ac4d209b236edfe28053805071
abdelhalim <rioman19@gmail.com>
2024-10-11 09:48:17 +0100

    chore: enhance component

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -1,46 +1,70 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { circOut } from 'svelte/easing';
+	import { bisector, pointer } from 'd3';
+	import gsap from 'gsap';
 	import { getChartfullContext } from '$lib/chartfull';
 	import { portal } from '$lib/actions';
-	import gsap from 'gsap';
-	import { backOut } from 'svelte/easing';
 
 	const chartfull_context = getChartfullContext();
 
 	let element: HTMLDivElement;
-	let x = 0;
-	let y = 0;
+	export let accessor: (d: any) => any;
+	export let x: [(v: any) => number, (d) => any];
+	export let y: [(v: any) => number, (d) => any];
+	export let data = [];
+
+	let klass = '';
+	export { klass as class };
+
+	let offset_x = 0;
+	let offset_y = 0;
+
+	let x_value;
+	let y_value;
+
+	let x_scaled = 0;
+	let y_scaled = 0;
+
+	let value = undefined;
 
 	onMount(() => {
 		const target_element = chartfull_context.root_element;
 		if (!target_element) {
 			throw new Error('target element is undefined');
 		}
-		console.log('Target Element: ', target_element);
+
+		const bisect = bisector(x[1]).center;
 
 		const onpointermove = (ev: PointerEvent) => {
-			x = ev.offsetX;
-			y = ev.offsetY;
+			const [xScale, xAccessor] = x;
+			const [yScale, yAccessor] = y;
 
-			gsap.to(element, { x, y, duration: 0.3, ease: backOut });
+			const i = bisect(data, xScale.invert(pointer(ev)[0]));
+
+			value = data[i];
+
+			gsap.to(element, {
+				x: xScale(xAccessor(value)) ?? 0,
+				y: yScale(yAccessor(value)),
+				duration: 0.3,
+				ease: circOut
+			});
 		};
 
-		console.log('register event listener');
 		target_element.addEventListener('pointermove', onpointermove);
 
 		return () => target_element.removeEventListener('pointermove', onpointermove);
 	});
-
-	export let open = false;
 </script>
 
 <div
-	class="chartfull-tooltip"
+	class="chartfull-tooltip {klass}"
 	use:portal={chartfull_context.html_layer_element}
 	bind:this={element}
 >
 	<div class="inner">
-		<slot />
+		<slot {value} />
 	</div>
 </div>
 
